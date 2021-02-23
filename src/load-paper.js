b1f0ec94b6225d882e0754dc0816eba670070164
Reinier Post <reinier.d.j.post@gmail.com>
2021-02-23 13:46:18 +0100

    Butt-ugly, but it works. Future commits will polish this up.

---
+++ src/load-paper.js
@@ -0,0 +1,1 @@
+import * as paper from 'paper';
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.html',
+    tweak: './src/load-paper.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      //title: 'Paper.js with webpack demo',
+      template: 'src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+};
